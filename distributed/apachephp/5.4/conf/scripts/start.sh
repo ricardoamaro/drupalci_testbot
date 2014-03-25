9ab0f0ec08a4b8e819fc82d01699585cfd61a813
Ricardo Amaro <mail@ricardoamaro.com>
2014-03-25 12:42:59 +0000

    added: test.info.yml logic to pass to the Drupal installer and to artifact the test

--- distributed/apachephp/5.4/conf/scripts/start.sh
+++ distributed/apachephp/5.4/conf/scripts/start.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/bin/bash -xe
 
 # Start apache
 echo "Operation [start]..."
@@ -17,15 +16,11 @@ mkdir /var/www/sites/default/private
 chmod -R 777 /var/www/sites/default/files
 chmod -R 777 /var/www/sites/default/private
 
-# Install the site. Enable Simpletest (are we going to use phing?)
 echo "Operation [install]..."
-cd /root/drupal-install && phing install -Dapp.installUrl='core/install.php?langcode=en&profile=testing'
-cd /root/drupal-install && phing enable:simpletest
-
-#Operation [install via drush]...
-#drush si command=cd sites/default/files/checkout && drush si -y --db-url=mysql://root@localhost/drupaltestbotmysql --clean-url=0 --account-name=admin --account-pass=drupal --account-mail=admin@example.com
-#cd sites/default/files/checkout && drush si -y --db-url=mysql://root@localhost/drupaltestbotmysql --clean-url=0 --account-name=admin --account-pass=drupal --account-mail=admin@example.com 2>&1
-
+#For now we use Drush to install the site but we are going to
+#move to other real browser installer
+#GET ALL INFO FROM /var/www/test.info.yml
+drush si -y --db-url=mysql://root@localhost/drupaltestbotmysql --clean-url=0 --account-name=admin --account-pass=drupal --account-mail=admin@example.com
 
 # Show the environment variables for debugging.
 echo "##################################################"
@@ -41,7 +36,3 @@ echo "##################################################"
 # Run the test suite.
 echo "Operation [run tests]..."
 sudo -E -u www-data -H sh -c "export TERM=linux && cd /var/www && php ./core/scripts/run-tests.sh --php `which php` --url 'http://localhost' --color --concurrency $CONCURRENCY --xml '/var/www/results' '$GROUPS'"
-
-echo "Operation [giving you a shell]..."
-/bin/bash
-
--- distributed/apachephp/5.4/run.sh
+++ distributed/apachephp/5.4/run.sh
@@ -6,10 +6,10 @@ DRUPALBRANCH=$(echo $DRUPALVERSION | awk -F. '{print $1}')
 IDENTIFIER="testid-iteration" #SHOULD be passed via argument
 REPODIR="/opt" #Change to the volume on the host
 
-
 git clone ${REPODIR}/drupal-${DRUPALBRANCH}/ ${REPODIR}/${IDENTIFIER}/
 cd ${REPODIR}/${IDENTIFIER}/ ; git checkout ${DRUPALVERSION}
 
+#Write all ENV VARIABLES to ${REPODIR}/${IDENTIFIER}/test.info.yml 
 
 sudo docker run -d=false -i=false -t=false -v=/var/log:/var/host_logs:ro -v=/${REPODIR}/${IDENTIFIER}/:/var/www:rw -t drupal/testbot-web
 
@@ -23,10 +23,6 @@ sudo docker run -d=false -p=80:80 -p=9000:22 -t -i -v /var/logs:/var/host_logs:r
 echo "Container: ${CONTAINER} started"
 sleep 30
 sudo docker logs ${CONTAINER}
-
-
 docker run -d=false -name=drupaltestbot-web -p=80:80 -p=9000:22 -link=drupaltestbot-db:db -volumes-from=my-docroot drupaltestbot-apachephp /bin/bash
-
-
 docker rmi drupal/testbot-web
 
