cae72f160ccbeb56d24367ac03f87a19ecd65b5e
Ricardo Amaro <mail@ricardoamaro.com>
2014-03-25 14:09:54 +0000

    Write all ENV VARIABLES to test.info

--- distributed/apachephp/5.4/run.sh
+++ distributed/apachephp/5.4/run.sh
@@ -1,23 +1,33 @@
 #!/bin/bash -xe
-# Implies there is a "git clone --branch 7.x http://git.drupal.org/project/drupal.git" on /somedir/drupal-7/ (or 8)
 
-DRUPALVERSION="7.26" #SHOULD be passed via argument
-DRUPALBRANCH=$(echo $DRUPALVERSION | awk -F. '{print $1}') 
-IDENTIFIER="testid-iteration" #SHOULD be passed via argument
-REPODIR="/opt" #Change to the volume on the host
+# Implies there is a "git clone --branch 7.x http://git.drupal.org/project/drupal.git" on /somedir/drupal-7/ (or 8)
+if [[ -z "$DRUPALVERSION" ]]
+  then
+    DRUPALVERSION="7.26" #SHOULD be passed via argument
+    DRUPALBRANCH=$(echo $DRUPALVERSION | awk -F. '{print $1}') 
+    IDENTIFIER="testid-iteration" #SHOULD be passed via argument
+    REPODIR="/opt" #Change to the volume on the host
+fi
 
 git clone ${REPODIR}/drupal-${DRUPALBRANCH}/ ${REPODIR}/${IDENTIFIER}/
 cd ${REPODIR}/${IDENTIFIER}/ ; git checkout ${DRUPALVERSION}
 
-#Write all ENV VARIABLES to ${REPODIR}/${IDENTIFIER}/test.info.yml 
+#Write all ENV VARIABLES to ${REPODIR}/${IDENTIFIER}/test.info
+#For now format it in a source format
 
-sudo docker run -d=false -i=false -t=false -v=/var/log:/var/host_logs:ro -v=/${REPODIR}/${IDENTIFIER}/:/var/www:rw -t drupal/testbot-web
+echo "
+DRUPALVERSION=\"${DRUPALVERSION}\"
+DRUPALBRANCH=\"${DRUPALBRANCH}\"
+IDENTIFIER=\"${IDENTIFIER}\"
+REPODIR=\"${REPODIR}\"
+" > ${REPODIR}/${IDENTIFIER}/test.info
+
+docker run -d=false -i=false -t=false -link=drupaltestbot-db:db -v=/var/log:/var/host_logs:ro -v=/${REPODIR}/${IDENTIFIER}/:/var/www:rw -t drupal/testbot-web
 
 exit 0
 
-################
+#########TODELETE:#######
 
-#CONTAINER=$( 
 sudo docker run -d=false -p=80:80 -p=9000:22 -t -i -v /var/logs:/var/host_logs:ro -v /tmp/www:/var/www:ro drupal/testbot-web 
 
 echo "Container: ${CONTAINER} started"
