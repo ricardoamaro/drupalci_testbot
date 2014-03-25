f72da4fa4b995303d13a36b6388cbc756cb4e24c
Ricardo Amaro <mail@ricardoamaro.com>
2014-03-25 22:56:22 +0000

    use default variables if no vars are passed

--- distributed/apachephp/5.4/run.sh
+++ distributed/apachephp/5.4/run.sh
@@ -1,19 +1,17 @@
 #!/bin/bash -xe
 
 # Implies there is a "git clone --branch 7.x http://git.drupal.org/project/drupal.git" on /somedir/drupal-7/ (or 8)
-if [[ -z "$DRUPALVERSION" ]]
-  then
-    DRUPALVERSION="7.26" #SHOULD be passed via argument
-    DRUPALBRANCH=$(echo $DRUPALVERSION | awk -F. '{print $1}') 
-    IDENTIFIER="testid-iteration" #SHOULD be passed via argument
-    REPODIR="/opt" #Change to the volume on the host
-    DBUSER="drupaltestbot" 
-    DBPASS="drupaltestbotpw"
-    CONCURRENCY="4" 
-    TESTGROUPS="NonDefaultBlockAdmin" #TESTS TO RUN
-    RUNSCRIPT="php ./scripts/run-tests.sh --php /usr/bin/php --url 'http://localhost' --color --concurrency ${CONCURRENCY} --xml '/var/www/results' ${TESTGROUPS} "
-fi
+DRUPALVERSION="${DRUPALVERSION:-7.26}" #SHOULD be passed via argument or default to 7.26
+DRUPALBRANCH =$(echo $DRUPALVERSION | awk -F. '{print $1}')
+IDENTIFIER="${IDENTIFIER:-testid-iteration}" #SHOULD be passed via argument
+REPODIR=${REPODIR:-"/opt"} #Change to the volume on the host
+DBUSER=${DBUSER:-"drupaltestbot"} 
+DBPASS=${DBPASS:-"drupaltestbotpw"}  
+CONCURRENCY=${CONCURRENCY:-"4"} #How many cpus to use per run
+TESTGROUPS=${TESTGROUPS:-"--class NonDefaultBlockAdmin"} #TESTS TO RUN
+RUNSCRIPT=${RUNSCRIPT:-"php ./scripts/run-tests.sh --php /usr/bin/php --url 'http://localhost' --color --concurrency ${CONCURRENCY} --xml '/var/www/results' ${TESTGROUPS} "}
 
+#Clone the local repo to the run directory:
 git clone ${REPODIR}/drupal-${DRUPALBRANCH}/ ${REPODIR}/${IDENTIFIER}/
 cd ${REPODIR}/${IDENTIFIER}/ ; git checkout ${DRUPALVERSION}
 
@@ -27,9 +25,9 @@ IDENTIFIER=\"${IDENTIFIER}\"
 REPODIR=\"${REPODIR}\"
 DBUSER=\"${DBUSER}\"
 DBPASS=\"${DBPASS}\"
-RUNSCRIPT=\"${RUNSCRIPT}\"
 CONCURRENCY=\"${CONCURRENCY}\" 
 TESTGROUPS=\"${TESTGROUPS}\"
+RUNSCRIPT=\"${RUNSCRIPT}\"
 " > ${REPODIR}/${IDENTIFIER}/test.info
 
 docker run -d=false -i=true --link=drupaltestbot-db:db -v=/var/log:/var/host_logs:ro -v=${REPODIR}/${IDENTIFIER}/:/var/www:rw -t drupal/testbot-web /bin/bash
