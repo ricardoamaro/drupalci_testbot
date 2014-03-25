9473513848fba05c5e0063b7402aed07dc127a61
Ricardo Amaro <mail@ricardoamaro.com>
2014-03-25 17:36:11 +0000

    run-client.sh fixed

--- distributed/database/mysql/run-client.sh
+++ distributed/database/mysql/run-client.sh
@@ -5,7 +5,7 @@ TAG="mysql"
 CONTAINER_ID=$(docker ps | grep $TAG | awk '{print $1}')
 
 #IP=$(docker inspect $CONTAINER_ID | python -c 'import json,sys;obj=json.load(sys.stdin);print obj[0]["NetworkSettings"]["IPAddress"]')
-IP=$(docker inspect -format='{{.NetworkSettings.IPAddress}}' $CONTAINER_ID)
+IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $CONTAINER_ID)
 
 echo $IP
 
