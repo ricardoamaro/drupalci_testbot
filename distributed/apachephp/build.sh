e7b71e426ea2e915a2d6022d38591be71b0aaf35
Ricardo Amaro <mail@ricardoamaro.com>
2014-03-26 17:43:52 +0000

    remove duplicated var

--- distributed/apachephp/build.sh
+++ distributed/apachephp/build.sh
@@ -12,14 +12,14 @@ echo "DEFAULTING TO PHP5.4"
 } fi
 
 case $PHP in
-  5.3) echo "Building PHP5.3 drupal/testbot-web53"
-    VER="53"
+  5.3) echo "Building PHP5.3 drupal/testbot-web5.3"
+    VER="5.3"
     ;;
-  5.5) echo "Building PHP5.5 drupal/testbot-web55"
-    VER="55"
+  5.5) echo "Building PHP5.5 drupal/testbot-web5.5"
+    VER="5.5"
     ;;
-  *) echo "Building PHP5.4 drupal/testbot-web54"
-    VER="54"
+  *) echo "Building PHP5.4 drupal/testbot-web5.4"
+    VER="5.4"
     ;;
 esac
 
@@ -28,7 +28,5 @@ cp -r conf/ /tmp/php${VER}/conf/
 cp Dockerfile-PHP${VER} /tmp/php${VER}/Dockerfile
 time docker build -t drupal/testbot-web${VER} /tmp/php${VER}/.
 rm -rf /tmp/php${VER}/
-
-
 
 exit 0
