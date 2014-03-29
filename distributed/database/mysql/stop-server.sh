670fd55dde5f9fd5b2da67db9c0a7329f92b985c
Ricardo Amaro <mail@ricardoamaro.com>
2014-03-29 11:15:33 +0000

    added stop mysql script

---
+++ distributed/database/mysql/stop-server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+TAG="drupaltestbot-mysql"
+NAME="drupaltestbot-db"
+STALLED=$(docker ps -a | grep ${TAG} | grep Exit | awk '{print $1}')
+RUNNING=$(docker ps | grep ${TAG} | grep 3306 | awk '{print $1}')
+if [[ ${RUNNING} != "" ]]
+  then 
+    echo "Found database container: ${RUNNING} running..."
+    echo "Stoping..."
+    docker stop ${RUNNING}
+    exit 0
+  elif [[ $STALLED != "" ]]
+    then
+    echo "Found old container $STALLED. Removing..."
+    docker rm $STALLED
+    umount /tmp/tmp.*;
+fi
