01e1deb58a501e9d4e8c859353638399398fe12e
Ricardo Amaro <mail@ricardoamaro.com>
2014-03-29 14:19:22 +0000

    moved startup.sh to conf dir

---
+++ distributed/database/mysql/conf/startup.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+if [ ! -f /var/lib/mysql/ibdata1 ]; then
+
+    mysql_install_db
+
+    /usr/bin/mysqld_safe &
+    sleep 10s
+
+    echo "GRANT ALL ON *.* TO drupaltestbot@'%' IDENTIFIED BY 'drupaltestbotpw' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+
+    killall mysqld
+    sleep 10s
+fi
+
+/usr/bin/mysqld_safe
+
--- distributed/database/mysql/run-client.sh
+++ distributed/database/mysql/run-client.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-TAG="mysql"
-
+TAG="drupaltestbot-mysql"
 CONTAINER_ID=$(docker ps | grep $TAG | awk '{print $1}')
-
-#IP=$(docker inspect $CONTAINER_ID | python -c 'import json,sys;obj=json.load(sys.stdin);print obj[0]["NetworkSettings"]["IPAddress"]')
 IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' $CONTAINER_ID)
 
 echo $IP
-
-mysql -u drupaltestbot -p -h $IP
+mysql -u drupaltestbot -pdrupaltestbotpw -h $IP
 
