941ced4a8ccaedad38aea3614faa86f6a50aefa4
Ricardo Amaro <mail@ricardoamaro.com>
2014-04-02 15:30:27 +0100

    Added Mac/Vagrant support with new files

---
+++ Vagrantfile
@@ -0,0 +1,11 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu13_testbot"
+  config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/saucy/current/saucy-server-cloudimg-amd64-vagrant-disk1.box"
+  config.vm.provision :shell, :path => "provision.sh"
+  config.vm.network :private_network, ip: "192.168.42.42"
+end
---
+++ provision.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Name:         provision.sh
+#
+# Purpose:      quick start the vagrant box with all the things 
+#
+# Comments:          
+#
+# Usage:        vagrant up (on the repo root)
+#
+# Author:       Ricardo Amaro (mail@ricardoamaro.com)
+# Contributors: Jeremy Thorson jthorson
+#           
+# Bugs/Issues:  Use the issue queue on drupal.org
+#               IRC #drupal-infrastructure
+# 
+# Docs:         README.md for complete information
+#
+
+
+if [ -f /home/vagrant/modernizing_testbot__dockerfiles/.git/config ];
+then
+	echo "You seem to have this box installed"
+	echo "I'll just give you a shell..."
+	cd /home/vagrant
+	cd modernizing_testbot__dockerfiles
+	git pull
+	./build_all.sh update
+else 
+	echo "Installing and building the all thing..."
+	echo "on: $(hostname) with user: $(whoami)"
+	apt-get update
+	apt-get install -y git mc ssh gawk grep sudo 
+	cd /home/vagrant
+	git clone https://github.com/ricardoamaro/modernizing_testbot__dockerfiles.git
+	cd modernizing_testbot__dockerfiles
+	./build_all.sh cleanup
+fi 
+
+chown -fR vagrant:vagrant /home/vagrant >/dev/null
+echo "Box started, run vagrant halt to stop."
+echo 
+echo "To access the box and run tests, do:"
+echo "vagrant ssh"
+echo "cd modernizing_testbot__dockerfiles"
+echo 'Example: sudo TESTGROUPS="Bootstrap" DRUPALBRANCH="8.x" PATCH="/path/inthebox/to/your.patch,." ./run.sh'
