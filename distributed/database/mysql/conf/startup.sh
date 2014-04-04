11c99a2ce12786fd66ef123a179e002d9aba2c28
Ricardo Amaro <mail@ricardoamaro.com>
2014-04-04 02:56:12 +0100

    wait for mysql on very slow machines

--- distributed/database/mysql/conf/startup.sh
+++ distributed/database/mysql/conf/startup.sh
@@ -6,7 +6,8 @@ if [ ! -f /var/lib/mysql/ibdata1 ];
     mysql_install_db
     /usr/bin/mysqld_safe &
     PID="${!}"
-    sleep 10s
+    sleep 5s
+    while ! netcat -vz localhost 3306; do sleep 1; done
     echo "GRANT ALL ON *.* TO drupaltestbot@'%' IDENTIFIED BY 'drupaltestbotpw' WITH GRANT OPTION; SELECT User FROM mysql.user; FLUSH PRIVILEGES;" | mysql
     echo "Grants added"
     killall mysqld
