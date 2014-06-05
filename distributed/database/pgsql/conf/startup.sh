46840d033d615736cb35ef24ea707428a0a80e47
Jared K. Smith <jaredsmith@jaredsmith.net>
2014-06-05 18:54:52 -0500

    Initial support for startup.sh script in Dockerfile

---
+++ distributed/database/pgsql/conf/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -f /var/lib/mysql/ibdata1 ]; 
+    then
+    echo "rebuilding PostgreSQL database cluster"
+    # stop and drop the cluster
+    su -l postgres -c 'pg_dropcluster 9.1 main --stop'
+    # create a fresh new cluster
+    su -l postgres -c 'pg_createcluster 9.1 main --start'
+    # create a new user
+    su -l postgres -c 'PGPASSWORD=drupaltestbotpw createuser -d -E -l -R -S drupaltestbot'
+    # create a new default database for the user
+    su -l postgres -c 'createdb -O drupaltestbot drupaltestbot'
+    # stop the cluster
+    su -l postgres -c 'pg_ctlcluster 9.1 main stop'
+fi
+
+/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf
+echo "pgsql died at $(date)";
+
