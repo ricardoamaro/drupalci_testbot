45b274f05a95f9b6f114cb79530ace2ec762ed6e
Bastian Widmer <b.widmer@dasrecht.net>
2014-06-06 17:27:25 -0500

    whitespace chagnes

--- distributed/apachephp/run.sh
+++ distributed/apachephp/run.sh
@@ -2,18 +2,18 @@
 #
 # Name:         run.sh
 #
-# Purpose:      Start the testbot build runs 
+# Purpose:      Start the testbot build runs
 #
-# Comments:          
+# Comments:
 #
 # Usage:        sudo {VARIABLES} ./run.sh
 #
 # Author:       Ricardo Amaro (mail@ricardoamaro.com)
 # Contributors: Jeremy Thorson jthorson
-#           
+#
 # Bugs/Issues:  Use the issue queue on drupal.org
 #               IRC #drupal-infrastructure
-# 
+#
 # Docs:         README.md for complete information
 #
 
@@ -23,33 +23,33 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]
   echo -e "\n"
   echo -e "Usage:\t\t\e[38;5;148msudo {VARIABLES} ./run.sh\e[39m "
   echo -e "Purpose:\tRun the testbot containers and do tests."
-  echo 
+  echo
   echo "....................... VARIABLES .........................."
   echo -e "
-PATCH:         Local or remote Patches to be applied. 
+PATCH:         Local or remote Patches to be applied.
                Format: patch_location,apply_dir;patch_location,apply_dir;...
-DEPENDENCIES:  Contrib projects to be downloaded & patched. 
+DEPENDENCIES:  Contrib projects to be downloaded & patched.
                Format: module1,module2,module2...
 DEPENDENCIES_GIT  Format: gitrepo1,branch;gitrepo2,branch;...
 DEPENDENCIES_TGZ  Format: module1_url.tgz,module1_url.tgz,...
-DRUPALBRANCH:  Default is '8.x' 
-DRUPALVERSION: Default is '8' 
+DRUPALBRANCH:  Default is '8.x'
+DRUPALVERSION: Default is '8'
 TESTGROUPS:    Tests to run. Default is '--class NonDefaultBlockAdmin'
                A list is available at the root of this project.
-VERBOSE:       Default is 'false' 
+VERBOSE:       Default is 'false'
 DBTYPE:        Default is 'mysql' from either mysql/sqlite
-CMD:           Default is none. Normally use '/bin/bash' to debug the container 
-UPDATEREPO:    Force git pull of Drupal & Drush. Default is 'false' 
+CMD:           Default is none. Normally use '/bin/bash' to debug the container
+UPDATEREPO:    Force git pull of Drupal & Drush. Default is 'false'
 IDENTIFIER:    Automated Build Identifier. Only [a-z0-9-_.] are allowed
-REPODIR:       Default is 'HOME/testbotdata'  
-DRUPALREPO:    Default is 'http://git.drupal.org/project/drupal.git' 
-DRUSHREPO:     Default is 'https://github.com/drush-ops/drush.git' 
-BUILDSDIR:     Default is  equal to REPODIR 
-WORKSPACE:     Default is 'HOME/testbotdata/IDENTIFIER/' 
-DBUSER:        Default is 'drupaltestbot'  
-DBPASS:        Default is 'drupaltestbotpw' 
-DBCONTAINER:   Default is 'drupaltestbot-db' 
-PHPVERSION:    Default is '5.4' 
+REPODIR:       Default is 'HOME/testbotdata'
+DRUPALREPO:    Default is 'http://git.drupal.org/project/drupal.git'
+DRUSHREPO:     Default is 'https://github.com/drush-ops/drush.git'
+BUILDSDIR:     Default is  equal to REPODIR
+WORKSPACE:     Default is 'HOME/testbotdata/IDENTIFIER/'
+DBUSER:        Default is 'drupaltestbot'
+DBPASS:        Default is 'drupaltestbotpw'
+DBCONTAINER:   Default is 'drupaltestbot-db'
+PHPVERSION:    Default is '5.4'
 CONCURRENCY:   Default is '4'  #How many cpus to use per run
 RUNSCRIPT:     Default is 'php  RUNNER  --php /usr/bin/php --url 'http://localhost' --color --concurrency  CONCURRENCY  --verbose --xml '/var/workspace/results'  TESTGROUPS  | tee /var/www/test.stdout ' "
 echo -e "\n\nExamples:\t\e[38;5;148msudo {VARIABLES} ./run.sh\e[39m "
@@ -58,7 +58,7 @@ Run Action and Node tests, 2 LOCAL patches, using 4 CPUs, against D8:
 .....................................................................
 sudo TESTGROUPS=\"Action,Node\" CURRENCY=\"4\" DRUPALBRANCH=\"8.x\"  PATCH=\"/tmp/1942178-config-schema-user-28.patch,.;/tmp/1942178-config-schema-30.patch,.\" ./run.sh
 
-Run all tests using 4 CPUs, 1 core patch against D8:   
+Run all tests using 4 CPUs, 1 core patch against D8:
 .....................................................................
 sudo TESTGROUPS=\"--all\" CONCURRENCY=\"4\" DRUPALBRANCH=\"8.x\" PATCH=\"https://drupal.org/files/issues/1942178-config-schema-user-28.patch,.\" ./run.sh
 "
@@ -67,11 +67,11 @@ fi
 
 # Bellow there is a list of variables that you can override:
 
-IDENTIFIER=${IDENTIFIER:-"build_$(date +%Y_%m_%d_%H%M%S)"} 
+IDENTIFIER=${IDENTIFIER:-"build_$(date +%Y_%m_%d_%H%M%S)"}
 DRUPALBRANCH=${DRUPALBRANCH:-"8.x"}
 DRUPALVERSION=${DRUPALVERSION:-"$(echo $DRUPALBRANCH | awk -F. '{print $1}')"}
 UPDATEREPO=${UPDATEREPO:-"false"}
-REPODIR=${REPODIR:-"$HOME/testbotdata"} 
+REPODIR=${REPODIR:-"$HOME/testbotdata"}
 DRUPALREPO=${DRUPALREPO:-"http://git.drupal.org/project/drupal.git"}
 DRUSHREPO=${DRUSHREPO:-"https://github.com/drush-ops/drush.git"}
 BUILDSDIR=${BUILDSDIR:-"$REPODIR"}
@@ -79,8 +79,8 @@ WORKSPACE=${WORKSPACE:-"$BUILDSDIR/$IDENTIFIER/"}
 DEPENDENCIES=${DEPENDENCIES:-""}
 DEPENDENCIES_GIT=${DEPENDENCIES_GIT:-""}
 DEPENDENCIES_TGZ=${DEPENDENCIES_TGZ:-""}  #TODO
-PATCH=${PATCH:-""} 
-DBUSER=${DBUSER:-"drupaltestbot"} 
+PATCH=${PATCH:-""}
+DBUSER=${DBUSER:-"drupaltestbot"}
 DBPASS=${DBPASS:-"drupaltestbotpw"}
 DBTYPE=${DBTYPE:-"mysql"} #mysql/sqlite
 DBCONTAINER=${DBCONTAINER:-"drupaltestbot-db"}
@@ -91,13 +91,13 @@ PHPVERSION=${PHPVERSION:-"5.4"}
 CONCURRENCY=${CONCURRENCY:-"4"} #How many cpus to use per run
 TESTGROUPS=${TESTGROUPS:-"Bootstrap"} #TESTS TO RUN from https://api.drupal.org/api/drupal/classes/8
 
-# run-tests.sh place changes on 8.x 
+# run-tests.sh place changes on 8.x
 case $DRUPALVERSION in
   8) RUNNER="./core/scripts/run-tests.sh"
-     MODULESPATH="./modules" 
+     MODULESPATH="./modules"
     ;;
   *) RUNNER="./scripts/run-tests.sh"
-     MODULESPATH="./sites/all/modules" 
+     MODULESPATH="./sites/all/modules"
     ;;
 esac
 
@@ -107,7 +107,7 @@ case $VERBOSE in
   *) VERBO=""
     ;;
 esac
-    
+
 RUNSCRIPT=${RUNSCRIPT:-"php ${RUNNER} --php /usr/bin/php --url 'http://localhost' --color --concurrency ${CONCURRENCY} ${VERBO} --xml '/var/workspace/results'"}
 
 # Check if we have root powers
@@ -120,16 +120,16 @@ mkdir -p ${REPODIR}
 
 # Check if we have free disk space
 FREEDISK=$(df -m ${BUILDSDIR} | tail -n1 | awk '{print $4}')
-if (( $FREEDISK <= 100 )); 
-  then 
+if (( $FREEDISK <= 100 ));
+  then
     echo ""
-    echo "ERROR! Low disk space!"; 
+    echo "ERROR! Low disk space!";
     echo ""
     df -hT ${BUILDSDIR}
     echo ""
     echo "Try to clean up some disk space from ${BUILDSDIR}"
     echo "A minimum of 100MB is required..."
-    exit 1; 
+    exit 1;
 fi
 
 # If we are using mysql make sure the conatiner is there
@@ -152,13 +152,13 @@ fi
 
 #Ensure PHPVERSION is set
 case $PHPVERSION in
-  5.3) 
+  5.3)
     PHPVERSION="5.3"
     ;;
-  5.5) 
+  5.5)
     PHPVERSION="5.5"
     ;;
-  *) 
+  *)
     PHPVERSION="5.4"
     ;;
 esac
@@ -172,7 +172,7 @@ if $(docker images | grep -q testbot-web${PHPVERSION});
   echo "--------------------------------------------------------------------------------"
   else
   echo "--------------------------------------------------------------------------------"
-  echo "ERROR. Image testbot-web${PHPVERSION} needs to be built with:" 
+  echo "ERROR. Image testbot-web${PHPVERSION} needs to be built with:"
   echo "sudo ./build ${PHPVERSION}"
   echo "--------------------------------------------------------------------------------"
   exit 1
@@ -182,7 +182,7 @@ fi
 
 #Clone the local Drupal and Drush to the run directory:
 if [ -f ${REPODIR}/drupal/.git/config ];
-  then 
+  then
     echo "Local Drupal repo found on ${REPODIR}/drupal/"
   else
     echo ""
@@ -196,13 +196,13 @@ fi
 
 #install drush via composer on ${REPODIR}
 if [ -f ${REPODIR}/vendor/drush/drush/drush ];
-  then 
+  then
     echo "Local Drush found on ${REPODIR}/vendor/drush/drush/drush"
   else
 	cd ${REPODIR}
 	curl -sS https://getcomposer.org/installer | php -- --install-dir=${REPODIR}
 	${REPODIR}/composer.phar -d="${REPODIR}" global require drush/drush:dev-master
-fi 
+fi
 
 
 if [[ $UPDATEREPO = "true" ]]
@@ -228,13 +228,13 @@ git clone ${SB} --branch ${DRUPALBRANCH} ${REPODIR}/drupal/ ${BUILDSDIR}/${IDENT
 
 # Make it writable for artifacts
 mkdir -p  ${BUILDSDIR}/${IDENTIFIER}/results
-chmod a+w ${BUILDSDIR}/${IDENTIFIER}/results 
-chmod a+w ${BUILDSDIR}/${IDENTIFIER}/ 
+chmod a+w ${BUILDSDIR}/${IDENTIFIER}/results
+chmod a+w ${BUILDSDIR}/${IDENTIFIER}/
 
 #Change to the branch we would like to test
 if [[ ${DRUPALBRANCH} != "" ]]
   then
-    cd ${BUILDSDIR}/${IDENTIFIER}/ 
+    cd ${BUILDSDIR}/${IDENTIFIER}/
     git checkout ${DRUPALBRANCH} 2>&1 | head -n3
     echo ""
 fi
@@ -244,7 +244,7 @@ if [[ ${DBTYPE} = "sqlite" ]]
     DBLINK=""
 fi
 
-#DEPENDENCIES="module1,module2,module3" 
+#DEPENDENCIES="module1,module2,module3"
 #Get the dependecies
 if [[ $DEPENDENCIES = "" ]]
   then
@@ -252,14 +252,14 @@ if [[ $DEPENDENCIES = "" ]]
   else
       cd ${BUILDSDIR}/${IDENTIFIER}/
     for DEP in $(echo "$DEPENDENCIES" | tr "," "\n")
-      do 
+      do
       echo "Project: $DEP"
       ${REPODIR}/vendor/drush/drush/drush -y dl ${DEP}
-    done  
+    done
     echo ""
 fi
 
-#DEPENDENCIES_GIT="gitrepo1,branch;gitrepo2,branch" 
+#DEPENDENCIES_GIT="gitrepo1,branch;gitrepo2,branch"
 #Get the git dependecies
 if [[ $DEPENDENCIES_GIT = "" ]]
   then
@@ -273,11 +273,11 @@ if [[ $DEPENDENCIES_GIT = "" ]]
       read gurl gbranch <<<$(echo "${row}" | tr "," " ");
       echo "Git URL: $gurl Branch: $gbranch "
       git clone --branch $gbranch $gurl
-    done  
+    done
     echo ""
 fi
 
-#DEPENDENCIES_TGZ="module1_url.tgz,module1_url.tgz,module1_url.tgz" 
+#DEPENDENCIES_TGZ="module1_url.tgz,module1_url.tgz,module1_url.tgz"
 #Get the tgz dependecies
 if [[ $DEPENDENCIES_TGZ = "" ]]
   then
@@ -290,14 +290,14 @@ if [[ $DEPENDENCIES_TGZ = "" ]]
       do
       echo "TGZ URL: ${row}  "
       curl -s ${row} | tar xzf -
-    done  
+    done
     echo ""
 fi
 
-#PATCH="patch_url,apply_dir;patch_url,apply_dir;" 
+#PATCH="patch_url,apply_dir;patch_url,apply_dir;"
 #Apply Patch if any
 if [[ $PATCH = "" ]]
-  then 
+  then
     echo -e "NOTICE: \$PATCH variable has no patch to apply...\n"
   else
     ARRAY=($(echo "${PATCH}" | tr ";" "\n"))
@@ -305,10 +305,10 @@ if [[ $PATCH = "" ]]
       do
       read purl dir <<<$(echo "${row}" | tr "," " ");
       cd ${BUILDSDIR}/${IDENTIFIER}/${dir}/
-      if $(echo "$purl" | egrep -q "^http") ;  
-        then 
+      if $(echo "$purl" | egrep -q "^http") ;
+        then
           curl -s $purl > patch
-        else 
+        else
           cat  $purl > patch
       fi
       echo "Applying Patch: ${purl}"
@@ -324,7 +324,7 @@ if [[ $PATCH = "" ]]
         echo "  - You provided the correct apply directory."
         exit 1
       fi
-    done 
+    done
 fi
 
 
@@ -346,13 +346,13 @@ MODULESPATH=\"${MODULESPATH}\"
 PATCH=\"${PATCH}\"
 DBUSER=\"${DBUSER}\"
 DBPASS=\"${DBPASS}\"
-DBTYPE=\"${DBTYPE}\" 
+DBTYPE=\"${DBTYPE}\"
 DBCONTAINER=\"${DBCONTAINER}\"
 DBLINK=\"${DBLINK}\"
 CMD=\"${CMD}\"
 VERBOSE=\"${VERBOSE}\"
 PHPVERSION=\"${PHPVERSION}\"
-CONCURRENCY=\"${CONCURRENCY}\" 
+CONCURRENCY=\"${CONCURRENCY}\"
 RUNSCRIPT=\"${RUNSCRIPT}\"
 TESTGROUPS=\"${TESTGROUPS}\"
 " | tee ${BUILDSDIR}/${IDENTIFIER}/test.info
