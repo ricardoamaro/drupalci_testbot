64dddff8d624d428c7fc3be76e6dfdaa599ea288
Jared K. Smith <jaredsmith@jaredsmith.net>
2014-06-07 16:43:45 -0500

    Allow scripts to be run as a member of the "docker" group

--- distributed/database/pgsql/stop-server.sh
+++ distributed/database/pgsql/stop-server.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Check if we have root powers
-if [ `whoami` != root ]; then
+# Check if we are a member of the "docker" group or have root powers
+USERNAME=$(whoami)
+TEST=$(groups $USERNAME | grep -c '\bdocker\b')
+if [ $TEST -eq 0 ];
+then
+  if [ `whoami` != root ]; then
     echo "Please run this script as root or using sudo"
     exit 1
+  fi
 fi
 
 TAG="drupal/testbot-pgsql"
