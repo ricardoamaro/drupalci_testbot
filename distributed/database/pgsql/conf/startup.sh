517e91bf5d7dfaddd9d7e844ce64a677ea437a2a
Jared K. Smith <jaredsmith@jaredsmith.net>
2014-06-07 21:27:47 -0500

    Changes to fix the locale issue.  This should be working now

--- distributed/database/pgsql/conf/startup.sh
+++ distributed/database/pgsql/conf/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
-export LANG_ALL=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 if [ ! -z $(pg_lsclusters | grep -c ' main ') ]; 
     then
@@ -12,9 +13,9 @@ if [ ! -z $(pg_lsclusters | grep -c ' main ') ];
     pg_createcluster 9.1 main --start
 
     # create a new user
-    psql -c "CREATE USER drupaltestbot WITH PASSWORD 'drupaltestbotpw';"
+    psql -c "CREATE USER drupaltestbot WITH PASSWORD 'drupaltestbotpw' CREATEDB;"
     # create a new default database for the user
-    psql -c "CREATE DATABASE drupaltestbot1 OWNER drupaltestbot TEMPLATE template0 ENCODING='utf8' LC_CTYPE='en_US.UTF-8' LC_COLLATE='en_US.UTF-8';"
+    psql -c "CREATE DATABASE drupaltestbot OWNER drupaltestbot TEMPLATE DEFAULT ENCODING='utf8' LC_CTYPE='en_US.UTF-8' LC_COLLATE='en_US.UTF-8';"
     # stop the cluster
     pg_ctlcluster 9.1 main stop
     # allow md5-based password auth for IPv4 connections
