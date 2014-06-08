5b2a5a22faa79b2dfb80de38de40fd20acace409
Jared K. Smith <jaredsmith@jaredsmith.net>
2014-06-07 23:43:24 -0500

    Initial checking for a MariaDB image

---
+++ distributed/database/mariadb/run-server.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Check if we have root powers
+if [ `whoami` != root ]; then
+    echo "Please run this script as root or using sudo"
+    exit 1
+fi
+
+
+TAG="drupal/testbot-mariadb"
+NAME="drupaltestbot-db"
+STALLED=$(docker ps -a | grep ${TAG} | grep Exit | awk '{print $1}')
+RUNNING=$(docker ps | grep ${TAG} | grep 3306)
+if [[ $RUNNING != "" ]]
+  then 
+    echo "Found database container:" 
+    echo "$RUNNING already running..."
+    exit 0
+  elif [[ $STALLED != "" ]]
+    then
+    echo "Found old container $STALLED. Removing..."
+    docker rm $STALLED
+    if [ -d "/tmp/tmp.*" ]; then
+      rm -fr /tmp/tmp.* || /bin/true
+      umount -f /tmp/tmp.* || /bin/true
+      rm -fr /tmp/tmp.* || /bin/true
+    fi
+fi
+  
+TMPDIR=$(mktemp -d)
+mount -t tmpfs -o size=16000M tmpfs $TMPDIR
+
+docker run -d -p=3306 --name=${NAME} -v="$TMPDIR":/var/lib/mysql ${TAG}
+CONTAINER_ID=$(docker ps | grep ${TAG} | awk '{print $1}')
+
+#PORT=$(docker port $MYSQL_ID 3606 | cut -d":" -f2)
+#TAG="drupal/testbot-mariadb"
+
+echo "CONTAINER STARTED: $CONTAINER_ID"
+
+docker ps | grep "drupal/testbot-mariadb"
+
