aacfe56fee518ed710e4f1dbdf2b21eccbb7c734
Bastian Widmer <b.widmer@dasrecht.net>
2014-06-08 20:21:36 -0500

    Fixing Vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -8,7 +8,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box_url = "https://dl.dropboxusercontent.com/u/197673519/debian-7.2.0.box"
   config.vm.provision :shell, :path => "provision.sh"
   config.vm.network :private_network, ip: "192.168.42.42"
-  
+  config.vm.synced_folder ".", "/home/vagrant/modernizing_testbot__dockerfiles", type: "rsync"
   config.vm.define "testbot" do |testbot|
       testbot.vm.provider "virtualbox" do |v|
         v.customize [ "modifyvm", :id, "--cpus", "4" ]
--- provision.sh
+++ provision.sh
@@ -2,38 +2,35 @@
 #
 # Name:         provision.sh
 #
-# Purpose:      quick start the vagrant box with all the things 
+# Purpose:      quick start the vagrant box with all the things
 #
-# Comments:          
+# Comments:
 #
 # Usage:        vagrant up (on the repo root)
 #
 # Author:       Ricardo Amaro (mail@ricardoamaro.com)
 # Contributors: Jeremy Thorson jthorson
-#           
+#
 # Bugs/Issues:  Use the issue queue on drupal.org
 #               IRC #drupal-infrastructure
-# 
+#
 # Docs:         README.md for complete information
 #
 
 export HOME="/home/vagrant"
 
-#env
-
-if [ -f /home/vagrant/modernizing_testbot__dockerfiles/.git/config ];
+if [ -f /home/vagrant/modernizing_testbot__dockerfiles/PROVISIONED ];
 then
 	echo "You seem to have this box installed"
 	echo "I'll just give you a shell..."
 	swapon /var/swapfile
-	cd /home/vagrant
-	cd modernizing_testbot__dockerfiles
-	git pull
+	cd /home/vagrant/modernizing_testbot__dockerfiles
 	./build_all.sh update
-else 
+else
 	echo 'Defaults        env_keep +="HOME"' >> /etc/sudoers
 	echo "Installing and building the all thing..."
 	echo "on: $(hostname) with user: $(whoami) home: $HOME"
+	swapoff -a
 	dd if=/dev/zero of=/var/swapfile bs=1M count=2048
 	chmod 600 /var/swapfile
 	mkswap /var/swapfile
@@ -42,15 +39,14 @@ else
 	apt-get update
 	apt-get install -y git mc ssh gawk grep sudo htop mysql-client php5-cli
 	apt-get autoclean
-	cd /home/vagrant
-	git clone https://github.com/ricardoamaro/modernizing_testbot__dockerfiles.git
-	cd modernizing_testbot__dockerfiles
-	./build_all.sh cleanup
-fi 
+	cd /home/vagrant/modernizing_testbot__dockerfiles
+	./scripts/build_all.sh cleanup $database
+	touch PROVISIONED
+fi
 
-chown -fR vagrant:vagrant /home/vagrant 
+chown -fR vagrant:vagrant /home/vagrant
 echo "Box started, run vagrant halt to stop."
-echo 
+echo
 echo "To access the box and run tests, do:"
 echo "vagrant ssh"
 echo "cd modernizing_testbot__dockerfiles"
