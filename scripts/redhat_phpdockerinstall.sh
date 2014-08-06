8e3272720ef7cb1e6871d16572a81e7156a48769
Ricardo Amaro <mail@ricardoamaro.com>
2014-08-06 19:22:50 +0100

    ignore already existing epel-release-6-8

--- scripts/redhat_phpdockerinstall.sh
+++ scripts/redhat_phpdockerinstall.sh
@@ -3,7 +3,7 @@
 yum -y install php-cli
 
 #For Centos6
-rpm -iUvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -iUvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm | true
 yum update -y
 yum -y install docker-io
 service docker start
